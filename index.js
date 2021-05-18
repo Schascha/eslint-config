db2df5ae86fb2980132eb6726c6072cf26c092c5
Sascha Künstler <sascha.kuenstler@moccu.com>
2021-05-18 09:49:58 +0200

    Provide spacing rules

--- index.js
+++ index.js
@@ -3,17 +3,26 @@
 module.exports = {
 	extends: 'eslint:recommended',
 	rules: {
+		'keyword-spacing': 'error',
+		'no-multi-spaces': [
+			'error',
+			{
+				'ignoreEOLComments': true
+			}
+		],
 		'object-curly-spacing': ['error', 'never'],
 		'one-var': 'error',
 		'prefer-const': 'error',
 		'quotes': [
 			'error',
-			'single', {
+			'single',
+			{
 				'allowTemplateLiterals': true
 			}
 		],
 		'semi': 'error',
 		'semi-spacing': 'error',
+		'space-before-blocks': 'error',
 		'space-before-function-paren': ['error', 'never']
 	}
 };
