6d8908b8121af820af5b2a5eb54b6c4b8ad09fcf
Sascha Künstler <sascha.kuenstler@moccu.com>
2023-09-28 15:35:27 +0200

    Extend issues together with prettier

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 'use strict';
 
 module.exports = {
-	extends: 'eslint:recommended',
+	extends: [
+		'eslint:recommended',
+		'prettier'
+	],
 	rules: {
-		'keyword-spacing': 'error',
-		'no-multi-spaces': [
-			'error',
-			{
-				'ignoreEOLComments': true
-			}
-		],
-		'object-curly-spacing': ['error', 'never'],
-		'one-var': 'error',
-		'prefer-const': 'error',
-		'quotes': [
-			'error',
-			'single',
-			{
-				'allowTemplateLiterals': true
-			}
-		],
-		'semi': 'error',
-		'semi-spacing': 'error',
-		'space-before-blocks': 'error',
-		'space-before-function-paren': ['error', 'never']
+		'prefer-const': 'error'
 	}
 };
